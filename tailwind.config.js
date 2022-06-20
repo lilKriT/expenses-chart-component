15b60992569690cf1f0bac2c9ba0d8fe6b763b13
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-20 21:51:47 +0200

    Styling.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        softred: "hsl(25, 47%, 15%)",
+        softRed: "hsl(10, 79%, 65%)",
         cyan: "hsl(186, 34%, 60%)",
         darkBrown: "hsl(25, 47%, 15%)",
         mediumBrown: "hsl(28, 10%, 53%)",
