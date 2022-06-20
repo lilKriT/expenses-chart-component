10d305a5de1bc08cfd2e2db6e4b7a278925ebe0c
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-20 21:28:23 +0200

    Added styles to css.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["DM Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        softred: "hsl(25, 47%, 15%)",
+        cyan: "hsl(186, 34%, 60%)",
+        darkBrown: "hsl(25, 47%, 15%)",
+        mediumBrown: "hsl(28, 10%, 53%)",
+        cream: "hsl(27, 66%, 92%)",
+        veryPaleOrange: "hsl(33, 100%, 98%)",
+      },
+      fontSize: {
+        base: "1.125rem",
+      },
+    },
   },
   plugins: [],
 };
